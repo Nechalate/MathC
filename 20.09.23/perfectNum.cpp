52dc6a50f5fbb67bec746bbdfc96f44c6aa463a7
Nechalate <penpolpen@gmail.com>
2023-09-20 20:25:02 +0300

    perfect update

--- 20.09.23/perfectNum.cpp
+++ 20.09.23/perfectNum.cpp
@@ -5,7 +5,7 @@ using namespace std;
     bool Perfect (int num) 
     {
       int sum = 0;
-      for (int i = 1; i < num; i++) 
+      for (int i = 1; i <= num / 2; i++) 
       {
         if(num % i == 0) sum += i;
       }
