e94bc855b9d590f16437863e26f8219fb627f0d4
Nechalate <64017374+Nechalate@users.noreply.github.com>
2023-10-18 23:50:58 +0300

    Task3

---
+++ KontrolC++ 18.10.23/Task3/Kontrolnaya/Task3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int katet(int a, int b)
+{
+    return sqrt(b * b - a * a);
+}
+
+int ploshad(int a, int b)
+{
+    return (a * sqrt(b * b - a * a)) / 2;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int a, b;
+
+    cout << "Введите катет: "; cin >> a; 
+    cout << "Введите гипотенузу: "; cin >> b;
+    cout << katet(a, b) << endl;
+    cout << ploshad(a, b);
+}
+
